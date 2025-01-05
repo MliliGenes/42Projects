00d03e7eacbbc349d3854278295d5e5a0e9dcd40
MliliGenes <saad.elm.77@gmail.com>
2025-01-06 00:52:07 +0100

    it working

--- holy_range/checker.c
+++ holy_range/checker.c
@@ -1,29 +1,50 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   checker.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sel-mlil <sel-mlil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/31 14:06:51 by sel-mlil          #+#    #+#             */
-/*   Updated: 2025/01/06 00:04:24 by sel-mlil         ###   ########.fr       */
+/*   Created: 2025/01/04 18:00:56 by sel-mlil          #+#    #+#             */
+/*   Updated: 2025/01/06 00:47:39 by sel-mlil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/push_swap.h"
+#include "./includes/checker.h"
+#include "./includes/get_next_line.h"
+
+static void	check_sort(t_item *stack_a, t_item *stack_b)
+{
+	if (is_sorted(stack_a) && !stack_b)
+		ok();
+	else
+		ko();
+}
 
 int	main(int argc, char **argv)
 {
 	t_item	*stack_a;
 	t_item	*stack_b;
+	char	*move;
 
 	(void)argc;
 	stack_a = NULL;
 	stack_b = NULL;
+	move = NULL;
 	if (!parser(argv, &stack_a))
 		error();
 	else
-		sort(&stack_a, &stack_b);
+	{
+		while (1)
+		{
+			move = get_next_line(0);
+			if (!move)
+				return (error(), 1);
+			execution(&stack_a, &stack_b, move);
+			free(move);
+		}
+	}
+	check_sort(stack_a, stack_b);
 	clear_list(stack_a);
 	return (0);
 }
--- holy_range/push_swap.c
+++ holy_range/push_swap.c
@@ -1,50 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   checker.c                                          :+:      :+:    :+:   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sel-mlil <sel-mlil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/04 18:00:56 by sel-mlil          #+#    #+#             */
-/*   Updated: 2025/01/06 00:15:41 by sel-mlil         ###   ########.fr       */
+/*   Created: 2024/12/31 14:06:51 by sel-mlil          #+#    #+#             */
+/*   Updated: 2025/01/06 00:47:35 by sel-mlil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/checker.h"
-#include "../includes/get_next_line.h"
-
-static void	check_sort(t_item *stack_a, t_item *stack_b)
-{
-	if (is_sorted(stack_a) && !stack_b)
-		ok();
-	else
-		ko();
-}
+#include "./includes/push_swap.h"
 
 int	main(int argc, char **argv)
 {
 	t_item	*stack_a;
 	t_item	*stack_b;
-	char	*move;
 
 	(void)argc;
 	stack_a = NULL;
 	stack_b = NULL;
-	move = NULL;
 	if (!parser(argv, &stack_a))
 		error();
 	else
-	{
-		while (1)
-		{
-			move = get_next_line(0);
-			if (!move)
-				return (error(), 1);
-			execution(&stack_a, &stack_b, move);
-			free(move);
-		}
-	}
-	check_sort(stack_a, stack_b);
+		sort(&stack_a, &stack_b);
 	clear_list(stack_a);
 	return (0);
 }
